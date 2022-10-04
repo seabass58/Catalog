66768ebacdec142198cd4d00f8ed5a4e6548d46e
seabass58 <sebastianortiz5813@gmail.com>
2022-10-04 10:17:11 -0500

    POST, PUT, DELETE

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -43,5 +43,55 @@ namespace Catalog.Controllers
 
             return item.AsDto();
         }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
+        }
+
+        //PUT /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdatedItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item updateItem = existingItem with {Name = itemDto.Name, Price = itemDto.Price};
+
+            repository.UpdateItem(updateItem);
+
+            return NoContent();
+        }
+
+        //Delete /items/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+                var existingItem = repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+
+            return NoContent();
+        }
     }
 }
---
+++ Dtos/UpdatedItemDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Dtos
+{
+    public record UpdatedItemDto
+    {
+        [Required]
+        public string Name { get; init; }
+
+        [Required]
+        [Range(1, 1000)]
+        public decimal Price { get; init; }
+    }
+}
