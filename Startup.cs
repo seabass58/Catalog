8f0e8c6b03e26dfb565c3ac07957b749242fcee3
seabass58 <sebastianortiz5813@gmail.com>
2022-10-05 17:11:21 -0500

    Docker

--- Startup.cs
+++ Startup.cs
@@ -74,8 +74,11 @@ namespace Catalog
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
             }
 
+            if ( env.IsDevelopment())
+            {
             app.UseHttpsRedirection();
-
+            }
+            
             app.UseRouting();
 
             app.UseAuthorization();
